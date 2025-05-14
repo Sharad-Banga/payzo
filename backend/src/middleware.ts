ab1d26dfb992ab67c48d9b5c53741107fba335bb
Sharad Banga <sharadiit03@gmail.com>
2025-05-14 15:30:22 +0530

    adding collections

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -6,17 +6,24 @@ export const authMiddleware = (req :Request,res:Response,next:NextFunction)=>{
 
   const header = req.headers["authorization"];
 
-  const decoded  = jwt.verify(header as string , JWT_SECRET);
+  try{
+      const decoded  = jwt.verify(header as string , JWT_SECRET);
 
-  if(decoded){
-    //@ts-ignore
-    req.userId = decoded.id;
-    next();
+      if(decoded){
+        //@ts-ignore
+        req.userId = decoded.id;
+        next();
+      }
+
+      else{
+        res.send({
+          message:"not logged in"
+        })
+      }
   }
-
-  else{
-    res.send({
-      message:"not logged in"
+  catch(err){
+    return res.json({
+      "message" : "accha nhi lg rha na"
     })
   }
 }
